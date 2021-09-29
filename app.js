380020e620b02c3493f329176a85bf9dd556f929
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-09-29 15:56:35 -0700

    unleashed the beasts into the den, let the beasts roar

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 // import functions and grab DOM elements
+const hothImg = document.getElementById('hoth');
+const catSound = document.getElementById('cat-sound');
 
+const mouthImg = document.getElementById('mouth');
+const dogSound = document.getElementById('dog-sound');
+
+const rancorImg = document.getElementById('rancor');
+const horseSound = document.getElementById('horse-sound');
 // initialize global state
 
 // set event listeners 
+hothImg.addEventListener('click', ()=>{
+    catSound.play();
+
+});
+
+mouthImg.addEventListener('click', ()=>{
+    dogSound.play();
+
+});
+
+rancorImg.addEventListener('click', ()=>{
+    horseSound.play();
+});
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
